87b8513932daa65d5003c8808d9510fd49f900a7
Your Name <you@example.com>
2020-04-21 16:09:57 +0900

    ①autopep8 flask8 で修正

--- reserve.py
+++ reserve.py
@@ -1,19 +1,20 @@
 # 日付操作系 ライブラリ
-import time
+# import time
 
 # WebDriver系
-from selenium import webdriver
+# from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import chromedriver_binary  # Adds chromedriver binary to path
 
-import subs.datesub as datesub
+# import chromedriver_binary  # Adds chromedriver binary to path
+
+# import subs.datesub as datesub
 
 # data, 辞書 インポート
-import data.data as data
+# import data.data as data
 import data.data as dic
-from data.data import room_data, room_datum
+# from data.data import room_data, room_datum
 
 
 def reserve_room(self, rsv_list):
@@ -25,7 +26,7 @@ def reserve_room(self, rsv_list):
         result_msg += self._login(login_name)
 
         # ログインできなかったら コンティニュー
-        if self.login_state == False:
+        if self.login_state is False:
             continue
 
         # 抽選申し込み状況・結果確認
@@ -37,7 +38,9 @@ def reserve_room(self, rsv_list):
     return result_msg
 
 # 日付リスト で指定された 予約を実行
-def _reserve_room( self, rsv_list ):
+
+
+def _reserve_room(self, rsv_list):
 
     result_msg = ""
     for rsv_data in rsv_list:
@@ -50,15 +53,14 @@ def _reserve_room( self, rsv_list ):
         self.wait.until(EC.element_to_be_clickable((By.ID, 'goBtn')))
         self.driver.execute_script(script_str)
 
-
         # 指定の施設を開く
-        #chorus_room = rsv_data.bname+"／"+rsv_data.iname
+        # chorus_room = rsv_data.bname+"／"+rsv_data.iname
         chorus_room = rsv_data.room
         icd = dic.room_icd[chorus_room]
         bcd = dic.room_bcd[chorus_room]
 
         url = "https://www.fureai-net.city.kawasaki.jp/user/view/user/rsvEmptyState.html?"
-        url =url + "bcd="+bcd+"&icd="+icd
+        url = url + "bcd=" + bcd + "&icd=" + icd
         self.driver.get(url)
 
         # Webページ上の 対象施設の 日付の更新
@@ -66,15 +68,14 @@ def _reserve_room( self, rsv_list ):
         rsvMonth = rsv_data.month
         rsvDay = rsv_data.day
 
-        day_string = str(rsvYear)+","+str(rsvMonth)+","+str(rsvDay)
+        day_string = str(rsvYear) + "," + str(rsvMonth) + "," + str(rsvDay)
         script_str = "javascript:selectCalendarDate(" + \
-            day_string+");return false;"
+            day_string + ");return false;"
 
-        print('Script:'+script_str)
-
+        print('Script:' + script_str)
 
         # vvv ちょっと待ってからスクリプト実行
-        #time.sleep(0.2)
+        # time.sleep(0.2)
 
         self.wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'calclick')))
         self.driver.execute_script(script_str)
@@ -102,9 +103,8 @@ def _reserve_room( self, rsv_list ):
         self.driver.execute_script(script_str)
 
         # --- 必要なパラメータの ローカル変数への 登録 ---
-        chorus_room = rsv_data.bname+"／"+rsv_data.iname
+        chorus_room = rsv_data.bname + "／" + rsv_data.iname
 
-        #print( room_data[-1] )
+        # print( room_data[-1] )
 
     return result_msg
-
